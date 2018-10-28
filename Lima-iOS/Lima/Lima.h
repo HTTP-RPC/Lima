643a8ceca4d7f08f983588c4e67bdb5c7de7f07d
Greg Brown <gk_brown@icloud.com>
2018-10-28 11:58:41 -0400

    Add workspace and stub out iOS/tvOS projects.

---
+++ Lima-iOS/Lima/Lima.h
@@ -0,0 +1,19 @@
+//
+//  Lima.h
+//  Lima
+//
+//  Created by Greg Brown on 10/28/18.
+//  Copyright © 2018 Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lima.
+FOUNDATION_EXPORT double LimaVersionNumber;
+
+//! Project version string for Lima.
+FOUNDATION_EXPORT const unsigned char LimaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lima/PublicHeader.h>
+
+
