fb8fa9955cbca8117dbd213f3b43b5f9a9b5abb0
Greg Brown <gk_brown@icloud.com>
2018-10-31 17:25:20 -0400

    Add layout parameters to view initializers.

--- Lima-iOS/Lima/UIKit+Lima.swift
+++ Lima-iOS/Lima/UIKit+Lima.swift
@@ -17,6 +17,8 @@ import UIKit
 public extension UILabel {
     public convenience init(text: String? = nil,
         textAlignment: NSTextAlignment = .natural, textColor: UIColor? = nil, font: UIFont? = nil,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UILabel) -> Void)? = nil) {
         self.init()
 
@@ -24,6 +26,9 @@ public extension UILabel {
         self.textAlignment = textAlignment
         self.textColor = textColor
         self.font = font
+
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -33,12 +38,20 @@ public extension UIImageView {
     public convenience init(image: UIImage? = nil,
         contentMode: UIControl.ContentMode = .scaleToFill,
         tintColor: UIColor? = nil,
+        width: CGFloat = .nan, height: CGFloat = .nan,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UIImageView) -> Void)? = nil) {
         self.init()
 
         self.image = image
         self.contentMode = contentMode
         self.tintColor = tintColor
+
+        self.width = width
+        self.height = height
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -48,6 +61,8 @@ public extension UIButton {
     public convenience init(type: UIButton.ButtonType,
         title: String? = nil, image: UIImage? = nil,
         tintColor: UIColor? = nil,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UIButton) -> Void)? = nil) {
         self.init(type: type)
 
@@ -55,6 +70,9 @@ public extension UIButton {
         setImage(image, for: .normal)
 
         self.tintColor = tintColor
+
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -67,6 +85,9 @@ public extension UITextField {
         autocorrectionType: UITextAutocorrectionType = .default,
         autocapitalizationType: UITextAutocapitalizationType = .sentences,
         isSecureTextEntry: Bool = false,
+        width: CGFloat = .nan,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UITextField) -> Void)? = nil) {
         self.init()
 
@@ -77,16 +98,27 @@ public extension UITextField {
         self.autocapitalizationType = autocapitalizationType
         self.isSecureTextEntry = isSecureTextEntry
 
+        self.width = width
+        self.weight = weight
+        self.anchor = anchor
+
         callback?(self)
     }
 }
 
 public extension UIDatePicker {
     public convenience init(datePickerMode: UIDatePicker.Mode = .dateAndTime,
+        height: CGFloat = .nan,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UIDatePicker) -> Void)? = nil) {
         self.init()
 
         self.datePickerMode = datePickerMode
+
+        self.height = height
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -107,10 +139,15 @@ public extension UISwitch {
 
 public extension UISegmentedControl {
     public convenience init(tintColor: UIColor? = nil,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UISegmentedControl) -> Void)? = nil) {
         self.init()
 
         self.tintColor = tintColor
+
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -118,11 +155,16 @@ public extension UISegmentedControl {
 
 public extension UISlider {
     public convenience init(minimumValue: Float = 0.0, maximumValue: Float = 1.0,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UISlider) -> Void)? = nil) {
         self.init()
 
         self.minimumValue = minimumValue
         self.maximumValue = maximumValue
+
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -131,12 +173,17 @@ public extension UISlider {
 public extension UIStepper {
     public convenience init(minimumValue: Double = 0.0, maximumValue: Double = 100.0,
         stepValue: Double = 1.0,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UIStepper) -> Void)? = nil) {
         self.init()
 
         self.minimumValue = minimumValue
         self.maximumValue = maximumValue
         self.stepValue = stepValue
+
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -145,11 +192,16 @@ public extension UIStepper {
 public extension UIPageControl {
     public convenience init(pageIndicatorTintColor: UIColor? = nil,
         currentPageIndicatorTintColor: UIColor? = nil,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UIPageControl) -> Void)? = nil) {
         self.init()
 
         self.pageIndicatorTintColor = pageIndicatorTintColor
         self.currentPageIndicatorTintColor = currentPageIndicatorTintColor
+
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -158,10 +210,15 @@ public extension UIPageControl {
 public extension UIActivityIndicatorView {
     public convenience init(style: UIActivityIndicatorView.Style,
         color: UIColor? = nil,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UIActivityIndicatorView) -> Void)? = nil) {
         self.init(style: style)
 
         self.color = color
+
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -170,11 +227,16 @@ public extension UIActivityIndicatorView {
 public extension UIProgressView {
     public convenience init(progressTintColor: UIColor? = nil,
         trackTintColor: UIColor? = nil,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UIProgressView) -> Void)? = nil) {
         self.init()
 
         self.progressTintColor = progressTintColor
         self.trackTintColor = trackTintColor
+
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
@@ -184,6 +246,9 @@ public extension UITextView {
     public convenience init(textContainer: NSTextContainer? = nil,
         isEditable: Bool = true, isSelectable: Bool = true,
         textAlignment: NSTextAlignment = .natural, textColor: UIColor? = nil, font: UIFont? = nil,
+        width: CGFloat = .nan, height: CGFloat = .nan,
+        weight: CGFloat = .nan,
+        anchor: LMAnchor = [],
         _ callback: ((UITextView) -> Void)? = nil) {
         self.init(frame: CGRect(), textContainer: textContainer)
 
@@ -192,6 +257,11 @@ public extension UITextView {
         self.textAlignment = textAlignment
         self.textColor = textColor
         self.font = font
+
+        self.width = width
+        self.height = height
+        self.weight = weight
+        self.anchor = anchor
 
         callback?(self)
     }
